5429534fc79237fb175aa8f7888565dd14092d9d
Hornwitser <github@hornwitser.no>
2015-11-25 14:02:09 +0100

    Add name method to ConsoleBridge
    Add method to name an id, be it a user, bridge, or the manager itself.

--- yetibridge/bridge/console.py
+++ yetibridge/bridge/console.py
@@ -17,12 +17,27 @@ class ConsoleBridge(BaseBridge):
             command = input()
             self.send_event('bridge_command', command, 'console')
 
+    def name(self, item_id):
+        try:
+            return self.users[item_id]
+        except KeyError:
+            pass
+
+        try:
+            return self._manager._bridge_name(item_id)
+        except ValueError:
+            pass
+
+        if item_id == id(self._manager):
+            return 'manager'
+
+        return str(item_id)
+
     def ev_user_join(self, event, user_id, name):
         self.users[user_id] = name
-        bridge_name = self._manager._bridge_name(event.bridge_id)
-        print("{}: user '{}' joined".format(bridge_name, name))
+        print("{}: user '{}' joined".format(self.name(event.bridge_id), name))
 
     def ev_user_leave(self, event, user_id):
-        bridge_name = self._manager._bridge_name(event.bridge_id)
-        print("{}: user '{}' joined".format(bridge_name, self.users[user_id]))
+        print("{}: user '{}' left".format(self.name(event.bridge_id),
+                                          self.users[user_id]))
         del self.users[user_id]
